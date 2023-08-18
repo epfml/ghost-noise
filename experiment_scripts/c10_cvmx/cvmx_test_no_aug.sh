9086900fd47d5c32642593e43064d31102375fcc
Atli Kosson <32368417+atkos@users.noreply.github.com>
2023-08-18 13:33:03 +0000

    Switch to $USER

--- experiment_scripts/c10_cvmx/cvmx_test_no_aug.sh
+++ experiment_scripts/c10_cvmx/cvmx_test_no_aug.sh
@@ -7,7 +7,7 @@ wd=5e-2
 # Setting 0, no augmentation
 for seed in 42 43 44
 do
-    PYTHONPATH=$PYTHONPATH:$BASE_DIR torchrun --nproc_per_node 1 train.py --output /home/kosson/runs/timm/ --checkpoint-hist 1 \
+    PYTHONPATH=$PYTHONPATH:$BASE_DIR torchrun --nproc_per_node 1 train.py --output /home/$USER/runs/timm/ --checkpoint-hist 1 \
         --model convmixer_cifar --model-kwargs kernel_size=5 patch_size=2 \
         --data-dir /home/$USER/datasets --dataset torch/CIFAR10 --dataset-download --num-classes 10 --pin-mem --input-size 3 32 32 --mean 0.4914 0.4822 0.4465 --std 0.2023 0.1994 0.2010 --smoothing 0.0 --train-split "train" --val-split "test" \
         --train-transforms 'PILToTensor={}' \
@@ -16,7 +16,7 @@ do
         --log-wandb --wandb-kwargs project=nodo_cvmx name="\"test-c10-cvmx-BG3-b0-lr$lr-seed$seed\"" 
 
     gbs=192
-    PYTHONPATH=$PYTHONPATH:$BASE_DIR torchrun --nproc_per_node 1 train.py --output /home/kosson/runs/timm/ --checkpoint-hist 1 \
+    PYTHONPATH=$PYTHONPATH:$BASE_DIR torchrun --nproc_per_node 1 train.py --output /home/$USER/runs/timm/ --checkpoint-hist 1 \
         --model convmixer_cifar --model-kwargs kernel_size=5 patch_size=2 norm_cfg="[{\"type\":\"norm\",\"subtype\":\"bn2d\",\"dkwargs\":{\"affine\":False}},{\"type\":\"noise\",\"subtype\":\"ghost_noise_injector_rep\",\"dkwargs\":{\"batch_size\":$gbs}},{\"type\":\"gain\",\"subtype\":\"standard\"},{\"type\":\"bias\",\"subtype\":\"standard\"}]" \
         --data-dir /home/$USER/datasets --dataset torch/CIFAR10 --dataset-download --num-classes 10 --pin-mem --input-size 3 32 32 --mean 0.4914 0.4822 0.4465 --std 0.2023 0.1994 0.2010 --smoothing 0.0 --train-split "train" --val-split "test" \
         --train-transforms 'PILToTensor={}' \
